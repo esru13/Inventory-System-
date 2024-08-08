c2c4f7ae31c6a41f8230df99c97b173fabe282c5
kkiduss <kidustesfaye92@gmail.com>
2024-08-08 12:47:48 +0300

    models

--- Inventory_Sys/app/Models/User.php
+++ Inventory_Sys/app/Models/User.php
@@ -11,16 +11,19 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+     
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+      
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -42,4 +45,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
