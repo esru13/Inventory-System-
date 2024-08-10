4801dac15c0d5857051e9f1619fc71b55f4b4b71
esru13 <esraelb313@gmail.com>
2024-08-11 02:33:12 +0300

    model

---
+++ Inventory/app/Models/BusinessOwner.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class BusinessOwner extends Model
+{
+    use HasFactory, HasApiTokens;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'company_name',
+        'business_type',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
